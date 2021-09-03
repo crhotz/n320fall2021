a700a3b5b0e2c6022ceaf8e6f775678e7d0f676e
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-02 23:34:36 -0400

    notes and stuff from Travis p5 vid

---
+++ Labs/Lab1/app.js
@@ -0,0 +1,44 @@
+//circle class
+class Circle {
+    constructor(cx, cy, radius, color) {
+        this.x = cx;
+        this.y = cy;
+        this.radius = radius;
+        this.color = color;
+        this.speed = 1 + Math.random() * 5;
+    }
+
+    //encapsulating
+    update() {  //method that moves and draws circle
+        this.y = this.y + this.speed;  //still in the class, keep vague/broad
+        this.speed = this.speed + .1; //accelerates rain as it falls
+        fill(this.color);
+        Circle(this.x, this.y, this.radius);
+    }
+}
+
+
+//circle objects in array
+var myCircles = [];
+myCircles[0] = new Circle(100, 10, 100, [166, 214, 222]);
+myCircles[1] = new Circle(300, 10, 100, [227, 137, 64]);
+
+
+function setup() {
+    createCanvas(800,600);
+}
+
+function draw() {
+    background(208, 213, 219);
+
+//    //uses myCircle.y to control other properties
+//    //object used to control stuff instead of singular variables
+//    myCircle.y ++;
+//    Circle(myCircle.x, myCircle.y, myCircle.radius);
+
+
+    //invoking the method
+    myCircles[0].update();
+    myCircles[1].update();
+
+}
