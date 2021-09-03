fb84c93be1b47a314f5ff4316a2916f7692746e2
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-03 01:28:02 -0400

    Update app.js

--- Labs/Lab1/app.js
+++ Labs/Lab1/app.js
@@ -1,13 +1,23 @@
 
 let cx = 20;
 
+class Circle {
+    constructor(cx, cy, radius, color) {
+        this.x = cx;
+        this.y = cy;
+        this.radius = radius;
+        this.color = color;
+    }
+}
+
+var raindrop = new Circle(50, 20, 30, [242, 249, 255]);
+
 function setup() {
-    console.log("setup")
     createCanvas(800,600);
 }
 
 function draw() {
-    console.log("draw")
+
     background(208, 213, 219);
 
     cx++;
