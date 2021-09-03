9345443a36064edfd5cdd36540c6a59b38e8f999
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-03 04:26:12 -0400

    Update app.js

--- Labs/Lab1/app.js
+++ Labs/Lab1/app.js
@@ -33,6 +33,15 @@ raindrops[12] = new Circle(38, 1, 18, [204, 231, 255]);
 raindrops[13] = new Circle(10, 7, 24, [204, 231, 255]);
 raindrops[14] = new Circle(136, 13, 17, [204, 231, 255]);
 raindrops[15] = new Circle(486, 6, 24, [204, 231, 255]);
+raindrops[16] = new Circle(773, 2, 19, [204, 231, 255]);
+raindrops[17] = new Circle(102, 9, 24, [204, 231, 255]);
+raindrops[18] = new Circle(679, 5, 17, [204, 231, 255]);
+raindrops[19] = new Circle(405, 15, 24, [204, 231, 255]);
+raindrops[20] = new Circle(294, 12, 16, [204, 231, 255]);
+raindrops[21] = new Circle(479, 4, 24, [204, 231, 255]);
+raindrops[22] = new Circle(583, 1, 18, [204, 231, 255]);
+raindrops[23] = new Circle(510, 7, 24, [204, 231, 255]);
+raindrops[24] = new Circle(376, 13, 17, [204, 231, 255]);
 
 
 
@@ -60,8 +69,15 @@ function draw() {
     raindrops[13].update();
     raindrops[14].update();
     raindrops[15].update();
-
-
+    raindrops[16].update();
+    raindrops[17].update();
+    raindrops[18].update();
+    raindrops[19].update();
+    raindrops[20].update();
+    raindrops[21].update();
+    raindrops[22].update();
+    raindrops[23].update();
+    raindrops[24].update();
 }
 
 
