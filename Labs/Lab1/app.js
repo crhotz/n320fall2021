c7b2ab334e58f1288b322387e3924c661bb8fe4a
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-03 01:14:45 -0400

    Update app.js

--- Labs/Lab1/app.js
+++ Labs/Lab1/app.js
@@ -1,56 +1,8 @@
-//circle class
-class Circle {
-    constructor(cx, cy, radius) {   //color
-        this.x = cx;
-        this.y = cy;
-        this.radius = radius;
-        //this.color = color;
-        this.speed = 1 + Math.random() * 5;
-    }
-
-    //encapsulating
-    update() {  //method that moves and draws circle
-        this.y = this.y + this.speed;  //still in the class, keep vague/broad
-        this.speed = this.speed + .1; //accelerates rain as it falls
-        //fill(this.color);
-        new Circle(this.x, this.y, this.radius);
-        console.log("hiii");
-    }
-}
-
-
-//circle objects in array
-//var myCircles = [];
-//myCircles[0] = new Circle(100, 10, 100, [166, 214, 222]);
-//myCircles[1] = new Circle(900, 10, 100, [227, 0, 4]);
-circtest = new Circle(200, 200, 30); //[23, 255, 67]
 
 function setup() {
-    createCanvas(800,600);
+    console.log("setup")
 }
 
 function draw() {
-    //background(208, 213, 219);
-    //fill(10, 10, 230);
-    rect(100, 100, 20, 40);
-    //new Circle(200, 200, 30, [123, 45, 67]);
-    console.log(circtest);
-
-    circle(30, 30, 20); //, [45,235,3]
-    
-    
-
-//    //uses myCircle.y to control other properties
-//    //object used to control stuff instead of singular variables
-//    myCircle.y ++;
-//    Circle(myCircle.x, myCircle.y, myCircle.radius);
-
-
-    //invoking the method
-//    myCircles[0].update();
-//    myCircles[1].update();
-    //console.log(myCircles[0].update());
-
-    circtest.update();
-    console.log(circtest.update());
+    console.log("draw")
 }
