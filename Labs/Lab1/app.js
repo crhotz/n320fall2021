2bb98a3484c62eb1744d9ed82caab007d8bc08a2
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-03 00:10:57 -0400

    Update app.js

--- Labs/Lab1/app.js
+++ Labs/Lab1/app.js
@@ -25,11 +25,11 @@ myCircles[1] = new Circle(900, 10, 100, [227, 0, 4]);
 
 
 function setup() {
-    createCanvas(800,600);
+    //createCanvas(800,600);
 }
 
 function draw() {
-    background(208, 213, 219);
+    //background(208, 213, 219);
     rect(100, 100, 20, 40);
     console.log(bgijfcm);
 
