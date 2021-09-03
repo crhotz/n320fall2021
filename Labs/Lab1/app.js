e34a32f327bcad52141ef39543fa39f00eea7a6f
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-03 01:20:37 -0400

    Update app.js

--- Labs/Lab1/app.js
+++ Labs/Lab1/app.js
@@ -3,10 +3,12 @@ let cx = 20;
 
 function setup() {
     console.log("setup")
+    createCanvas(800,600);
 }
 
 function draw() {
     console.log("draw")
+    background(208, 213, 219);
 
     cx++;
     circle(50, cx, 30);
