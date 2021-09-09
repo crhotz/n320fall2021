62fe91a60727c80599a2f03687a169f4be0bfc83
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-09 14:14:20 -0400

    Update app.js

--- OOP_Principles/app.js
+++ OOP_Principles/app.js
@@ -1,11 +1,14 @@
-//Abstraction
+//Abstraction- speed = distance/time
 
 
-//Encapsulation
+//Encapsulation- day of the month/year, can't have more than
 
 
-//Inheritance
+//Inheritance- vehicles have wheel #, color, seat #; trucks have beds, convertibles have roof material (metal, fabric)
 
 
-//Polymorphism
+//Polymorphism- 
+
+
+//Associations (has a, is a, belongs to)- 
 
