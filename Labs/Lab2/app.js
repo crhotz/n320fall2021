fffef2b918fb01caf1481532a43583f0aac2cbfa
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-09 22:48:32 -0400

    Update app.js

--- Labs/Lab2/app.js
+++ Labs/Lab2/app.js
@@ -9,7 +9,7 @@ class Instrument {
 
     //method for playing the instrument
     playing() {
-        console.log(this.family + this.play_verb + "at" + this.loudness);
+        console.log(this.family + " " + this.play_verb + " at " + this.loudness);
     }
 }
 
@@ -18,41 +18,43 @@ class Instrument {
 
 //woodwind class
 class Woodwind extends Instrument {
-    constructor(loudness, family, play_verb) {
+    constructor(loudness) {
         super(); //takes 3 arguments //encapsulate the 2
         this.family = "Woodwind";
         this.play_verb = "blows";
         this.loudness = loudness;
     }
-
-
 }
-let flute = new Woodwind(6);
-console.log(flute);
-
-class trout extends fish {
-    constructor(habitat, length, variety) {
-      super()
-      this.habitat = habitat
-      this.length = length
-      this.variety = variety
-    }
-  }
 
 //percussion class
 class Percussion extends Instrument {
-    constructor() {
+    constructor(loudness) {
         super();
+        this.family = "Percussion";
+        this.play_verb = "hits";
+        this.loudness = loudness;
     }
-
-    
 }
 
 //string class
 class String extends Instrument {
-    constructor() {
+    constructor(loudness) {
         super();
+        this.family = "String";
+        this.play_verb = "plucks";
+        this.loudness = loudness;
     }
-
-    
 }
+
+//array to test code
+let instruments = [];
+instruments[0] = new Woodwind(60);
+instruments[1] = new Percussion(110);
+instruments[2] = new String(70);
+
+//loop through array
+instruments.forEach(
+    function (instrument) {
+        instrument.playing();
+    }
+)
