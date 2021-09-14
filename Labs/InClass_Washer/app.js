0330a16255aa6586a78a8639c89c68e9fb720efb
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-14 14:29:33 -0400

    wm

--- Labs/InClass_Washer/app.js
+++ Labs/InClass_Washer/app.js
@@ -6,7 +6,7 @@ class WashingMachine {
     //properties for base color, frontCircle location, frontCircle radius,
         //front circleColor, w, h, dial position, dial r, dialColor
         baseColor = [0,0,0];
-        frontCircle_location = { x: 0, y: 0 };
+        frontCircle = { x: 0, y: 0 };
         frontCircle_radius = 10;
         frontCircle_color = [0,0,0];
         w = 5;
@@ -17,9 +17,20 @@ class WashingMachine {
 
     //constructor
     constructor() {
+        //washing machine
         let washEl = document.createElementNS("http://www.w3.org/2000/svg", "rect");
         washEl.setAttribute("fill", `rgb(${this.color[0]},${this.color[1]},${this.color[2]})`);
         console.log(`rgb(${this.color[0]},${this.color[1]},${this.color[2]})`);
+        washEl.setAttribute("rect", 10, 10, this.w, this.y);
+        base.appendChild(washEl);
+
+        //frontCircle
+        let frontCircleEl = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        frontCircleEl.setAttribute("cx", this.frontCircle.x);
+        frontCircleEl.setAttribute("cy", this.frontCircle.y);
+        frontCircleEl.setAttribute("r", this.frontCircle_radius);
+
+        //
     }
 
     //method for draw (draw image to screen w/ svg)
@@ -32,7 +43,7 @@ class WashingMachine {
     //base machine: WashingMachine instance (will be used for generation
         //of new machines->replaced when user selects new one)
     //currentMachines: array of all washingMachines generated in this gen.
-    //createMachines method: creates 10 new mach.s w/ var based on an input mach.'s
+    //createMachines method: creates 10 new mach.s(for loop) w/ var based on an input mach.'s
         //properties (rand +/- into those vars), draws those mach.s
     //selectMachine: selects a mach. and creates 10 new mach.s based off it
     //removeMachines: removes all mach.s on screen
