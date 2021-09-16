07a98bf304efea13a03f753a5a1f5d809cb6091c
crhotz <89478434+crhotz@users.noreply.github.com>
2021-09-16 14:15:25 -0400

    Update app.js

--- Labs/InClass_Washer/app.js
+++ Labs/InClass_Washer/app.js
@@ -29,7 +29,7 @@ class WashingMachine {
         console.log(`rgb(${this.baseColor[0]},${this.baseColor[1]},${this.baseColor[2]})`);
         base.appendChild(washEl);
 
-        console.log(`rect(this.rx, ry, w, h)`);
+        console.log(`rect(${this.rx}, ry, w, h)`);
 
         //frontCircle
         let frontCircleEl = document.createElementNS("http://www.w3.org/2000/svg", "circle");
